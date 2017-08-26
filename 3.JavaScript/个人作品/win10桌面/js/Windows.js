37c99a439574dd6d8a8d4d94737b9166b5f3427f
liufanglin <360082515@qq.com>
2017-08-26 18:30:29 +0800

    添加开始按钮功能

--- 3.JavaScript/个人作品/win10桌面/js/Windows.js
+++ 3.JavaScript/个人作品/win10桌面/js/Windows.js
@@ -12,6 +12,15 @@ window.onload=function(){
 				WindPopup.style.display='block';
 			};
 		};
-
+		var start=document.querySelector(".WindPopupLeft .start");//开始按钮
+		var starts=document.querySelectorAll(".WindPopupLeft li");//电源 设置  用户按钮
+		
+		starts[0].onclick=function(){
+			if(getComputedStyle(start).width==310+'px'){//开始键展开功能
+				start.style.width=48+'px';
+			}else{
+				start.style.width=310+'px';
+			}
+		}
 };
 	
