d9d67b3072f3cfe1f5de1c27c110d2fb8fe24926
liufanglin <360082515@qq.com>
2017-08-28 13:07:15 +0800

    修改样式

--- 3.JavaScript/个人作品/win10桌面/js/Windows.js
+++ 3.JavaScript/个人作品/win10桌面/js/Windows.js
@@ -13,6 +13,7 @@ window.onload=function(){
 			};
 			popups1.style.display='none';
 			start.style.width=48+'px';
+			
 		};
 		var start=document.querySelector(".WindPopupLeft .start");//开始按钮
 		var starts=document.querySelectorAll(".WindPopupLeft li");//电源 设置  用户按钮
@@ -20,21 +21,21 @@ window.onload=function(){
 		var a1=document.querySelectorAll(".popups1 a");
 		var txt1=['更改账户设置','锁定','注销'];
 		var txt2=['睡眠','关机','重启']
-		//开始键展开
+		
 		
 		starts[0].onclick=function(){
-			
 			if(popups1.style.display=='block'){
 				popups1.style.display='none';
 			}else{
 				if(getComputedStyle(start).width==310+'px'){
 					start.style.width=48+'px';
-					
 				}else{
 					start.style.width=310+'px';
+					
 				}
 			}
 			hover.style.display='none';
+			start.style.width==48+'px'?hove():' ';
 			
 		}
 		
@@ -48,8 +49,8 @@ window.onload=function(){
 			
 			for(var i=0;i<a1.length;i++){
 				a1[i].innerHTML=txt1[i];
+				a1[i].onmouseover=null;
 			}
-			
 			popups1.style.top=262+'px';
 		}
 		
@@ -65,9 +66,8 @@ window.onload=function(){
 				a1[i].innerHTML=txt2[i];
 			}
 			popups1.style.top=362+'px';
-		}
-		
-		//点击电源按钮后弹出层子项的hover功能
+			
+				//点击电源按钮后弹出层子项的hover功能
 		var txt4=['电脑保持开机状态,但耗电较少。应用会一致保持打开状态,这样在唤醒电脑后,可以立即恢复到你离开时的状态',
 				 '关闭所有应用,然后关闭电脑。','关闭所有应用,关闭电脑,然后重新打开电脑。']
 			for(var i=0;i<a1.length;i++){
@@ -93,13 +93,14 @@ window.onload=function(){
 					hover.style.textAlign=c;
 				}
 			}
+		}
+		
+	
 			
 		//最左边几个功能键的hover功能
 		var hover=document.querySelector(".hover");
-		
 		hove();
 		function hove(){
-			
 			var txt3=['展开','liufanglin368@163.com','设置','电源'];
 			for(var i=0;i<starts.length;i++){
 				starts[i].index=i;
@@ -124,5 +125,6 @@ window.onload=function(){
 				}
 			}
 		}
+		
 };
 	
